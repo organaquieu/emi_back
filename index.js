0f6431408964c7dbe9fa6896da32ab07f63976cc
iana ivanitskaia <yivanitskaya@list.ru>
2026-05-08 20:11:29 +0300

    some fixes

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express');
 
 const app = express();
@@ -12,19 +11,4 @@ app.get('/', (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
-=======
-const express = require('express');
-
-const app = express();
-const PORT = 3000;
-
-app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.json({ message: 'Backend works!' });
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
->>>>>>> acbf8526c2f17c39e020c1909c60d2a858310090
 });
